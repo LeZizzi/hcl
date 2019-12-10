08b581f54aabe55475d3a03a36f95ad54bd0c18a
olivier@nephele.ie <Moufa,123>
2019-12-10 14:45:42 +0000

    init

--- main.tf
+++ main.tf
@@ -6,3 +6,22 @@ credentials = file("service-account.json")
 
 }
 
+resource "google_compute_instance" "terra" {
+  machine_type = "f1-micro"
+  name = "terra"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  network_interface {network = google_compute_network.vpc_network.self_link
+    access_config {}
+  }
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+  auto_create_subnetworks = "true"
+} }
+}
+
