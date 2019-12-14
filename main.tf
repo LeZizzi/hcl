2c54e4d57b0c052a9b48d2baad15abeac7247511
olivier@nephele.ie <olivier@nephele.ie>
2019-12-14 16:10:18 +0000

    vpc

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "google_compute_subnetwork" "subnet" {
  name          = "${var.name}-subnet"
  ip_cidr_range = var.subnet_cidr
  network       = "${var.name}-vpc"
- depends_on    = ["google_compute_network.vpc"]
+ depends_on    = [google_compute_network.vpc]
  region      = var.region
 }
 // VPC firewall configuration
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,5 +1,5 @@
 region          = "europe-west2"
-gcp_project     = "sunlit-vortex-184612"
+gcp_project     = "terrabuild"
 credentials     = "service-account.json"
 name            = "dev"
 subnet_cidr = "10.10.0.0/24"
