fdcb366755d10d2f9f19f0300026d28f987f0603
olivier <olivier@nephele.ie>
2019-12-22 16:28:38 +0000

    target self but ports not seem open port problem turned out to be wifi problem with ssh ok over 4g hop

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,16 @@
 provider "google" {
 credentials = file("service-account.json")
-  project = "tera-83100"
-  region = "europe-west2-a"
-  zone = "europe-west1-b"
+  project = var.my_gcp_project
+  region = var.region
+  zone = var.zone
 }
 
 
-
+// Adding SSH Public Key in Project Meta Data
+resource "google_compute_project_metadata_item" "ssh-keys" {
+  key   = "ssh-keys"
+  value = "${var.public_key}"
+}
  resource "google_compute_project_metadata_item" "citizenx" {
    key = "ssh-keys"
    value = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5/tokirgrB62PuL05VrmGtDZ8jNI7d2eDzNtXJZIbhLsvKI8ILRuYrG77TbZm7UqcjootIFLBEj97yMrThb3V8ut3zsk31GQ8yhAepnijJA6QVx/tc0lmswzBNgVJLAAiia+dTvdHoAGV61oMAw5HlnppbiWQPio4XJkmPtgmskQwWlFh+cPCdvAMFDgIVwxZNbKKQvmfUE1L+NaUpqo2ln4DJxbsrAhwsUNsbkG+LyErfklF9yIPUzughTjKATz0umWAYUkIq9duW/9ks6geavd+9lu8CrLgoeAFjxb2P8IFkUPNK96xGkOBQt+PDSzraeHfsjuKJIunzBwa1Q3d citizenx"
