0033483caf99affef35a6fa3b992ff59b3e419c6
olivier <olivier@nephele.ie>
2019-12-22 16:53:04 +0000

    target self but ports not seem open port problem turned out to be wifi problem with ssh ok over 4g hop

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -14,7 +14,7 @@ variable "zone" {
 }
 
 variable "public_key" {
-  default = "Your_Public_Key_in_RSA_Format"
+  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5/tokirgrB62PuL05VrmGtDZ8jNI7d2eDzNtXJZIbhLsvKI8ILRuYrG77TbZm7UqcjootIFLBEj97yMrThb3V8ut3zsk31GQ8yhAepnijJA6QVx/tc0lmswzBNgVJLAAiia+dTvdHoAGV61oMAw5HlnppbiWQPio4XJkmPtgmskQwWlFh+cPCdvAMFDgIVwxZNbKKQvmfUE1L+NaUpqo2ln4DJxbsrAhwsUNsbkG+LyErfklF9yIPUzughTjKATz0umWAYUkIq9duW/9ks6geavd+9lu8CrLgoeAFjxb2P8IFkUPNK96xGkOBQt+PDSzraeHfsjuKJIunzBwa1Q3d citizenx"
 }
 
 // FIREWALL Variables
@@ -33,7 +33,7 @@ variable "image_fw" {
 }
 
 variable "machine_type_fw" {
-  default = "n1-standard-4"
+  default = "f1-micro"
 }
 
 variable "machine_cpu_fw" {
@@ -147,18 +147,3 @@ variable "fw" {
   
 }
 
-variable "ssh_keys" {
-  user = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC5/tokirgrB62PuL05VrmGtDZ8jNI7d2eDzNtXJZIbhLsvKI8ILRuYrG77TbZm7UqcjootIFLBEj97yMrThb3V8ut3zsk31GQ8yhAepnijJA6QVx/tc0lmswzBNgVJLAAiia+dTvdHoAGV61oMAw5HlnppbiWQPio4XJkmPtgmskQwWlFh+cPCdvAMFDgIVwxZNbKKQvmfUE1L+NaUpqo2ln4DJxbsrAhwsUNsbkG+LyErfklF9yIPUzughTjKATz0umWAYUkIq9duW/9ks6geavd+9lu8CrLgoeAFjxb2P8IFkUPNK96xGkOBQt+PDSzraeHfsjuKJIunzBwa1Q3d citizenx""
-  key= file("keypub")
-}
-
-variable "gce_ssh_user" {
-  description = "ssh user"
-  ssh = var.ssh_keys
-
-}
-
-variable "keypub" {
-  description = "public key file"
-
-}
